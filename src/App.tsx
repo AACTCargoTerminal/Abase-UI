1e1c4903ef71fc69c818a3687082e96befc39af6
heesub3229 <heesub3229@naver.com>
2026-07-07 17:28:00 +0900

    기본경로 제외 설정

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Root />} />
           <Route path="/sams/Main" element={<Main />} />
           <Route path="/intra/Main" element={<InfraMain />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         <Error />
         <Loading />
